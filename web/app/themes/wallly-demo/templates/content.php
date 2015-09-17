29db84fed0ea00f76bb03e12edd287625e25b1bc
Nur Sah Ketene <nursahketene@gmail.com>
2015-09-17 16:25:39 +0300

    started ndbs2015 theming

--- web/app/themes/wallly-demo/templates/content.php
+++ web/app/themes/wallly-demo/templates/content.php
@@ -1,22 +1,27 @@
 <div class="wallly_container_wrap">
-  <div class="wally_container_header row <?php echo isset($_GET['wallly_hide_header']) ? 'hidden' : ''; ?>">
+<!--   <div class="wally_container_header row <?php echo isset($_GET['wallly_hide_header']) ? 'hidden' : ''; ?>">
     <h1 class="col-xs-12"><img class="ict-logo" src="<?php echo get_template_directory_uri() . '/assets/images/ICT-musta-oranssi_04.png'; ?>"> <b class="pull-right">#ICTexpoHKI</b></h1>
-  </div>
+  </div> -->
   <div class="wallly_container hidden row"></div>
   <div class="wally_container_footer <?php echo isset($_GET['wallly_hide_footer']) ? 'hidden' : ''; ?>">
     <div class="row">
       <div class="col-sm-6">
         <ul class="list-inline footer-credits">
           <li>
-            <a href="http://itewiki.fi" target="_blank"><img class="bottom-logo" src="<?php echo get_template_directory_uri() . '/assets/images/logo-ite.png'; ?>"></a>
+            <a href="http://itewiki.fi" target="_blank"><img src="http://www.ndbsevents.com/wp-content/uploads/2015/06/NDBS-2015-logo-transparent-very-small.png" class="bottom-logo"></a>
+          </li>
+        </ul>
+      </div>
+      <div class="col-sm-6 text-right">
+        <ul class="list-inline footer-credits">
+          <li>
+            Powered By
           </li>
           <li>
             <a href="http://pixels.fi" target="_blank"><img class="bottom-logo" src="<?php echo get_template_directory_uri() . '/assets/images/logo_bc-pixels-v1.1-invert.svg'; ?>"></a>
           </li>
         </ul>
-      </div>
-      <div class="col-sm-6 text-right">
-        <h1><a href="http://itewiki.fi/some" target="_blank" class="bottom-link color-inverted">itewiki.fi/some</a></h1>
+        <!-- <h1><a href="http://itewiki.fi/some" target="_blank" class="bottom-link color-inverted">itewiki.fi/some</a></h1> -->
       </div>
     </div>
   </div>
