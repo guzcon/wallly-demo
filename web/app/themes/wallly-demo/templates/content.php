95e34ef22026a4e89c9696cffc1939bb47494084
Nur Sah Ketene <nursahketene@gmail.com>
2016-04-25 15:31:14 +0300

    * Wallly is not ready for ict 2016 * Added logos for pixels, ict and ite wiki * Added white icons for twitter and instagram * Content background is orange * Content text is white * Author image is moved to the top of the content * Author image size increased * Source icon moved to the top of the content * No image fill is implemented for icons. * Header inserted * Removed footer * Fixed a bug with twitter next_id

--- web/app/themes/wallly-demo/templates/content.php
+++ web/app/themes/wallly-demo/templates/content.php
@@ -1,9 +1,17 @@
 <div class="wallly_container_wrap">
-<!--   <div class="wally_container_header row <?php echo isset($_GET['wallly_hide_header']) ? 'hidden' : ''; ?>">
-    <h1 class="col-xs-12"><img class="ict-logo" src="<?php echo get_template_directory_uri() . '/assets/images/ICT-musta-oranssi_04.png'; ?>"> <b class="pull-right">#ICTexpoHKI</b></h1>
-  </div> -->
+  <div class="wally_container_header row <?php echo isset($_GET['wallly_hide_header']) ? 'hidden' : ''; ?>">
+    <div class="col-xs-4 wallly-img-header left">
+      <img class="header-icon" src="<?php echo plugins_url() . '/wallly/lib/ICTexpo.svg'; ?>">
+    </div>
+    <div class="col-xs-4 wallly-img-header mid">
+      <img class="header-icon" src="<?php echo plugins_url() . '/wallly/lib/itewiki.svg'; ?>">
+    </div>
+    <div class="col-xs-4 wallly-img-header right">
+      <img class="header-icon" src="<?php echo plugins_url() . '/wallly/lib/boonconPIXELS.svg' ?>">
+    </div>
+  </div>
   <div class="wallly_container hidden row"></div>
-  <div class="wally_container_footer <?php echo isset($_GET['wallly_hide_footer']) ? 'hidden' : ''; ?>">
+  <!-- <div class="wally_container_footer <?php echo isset($_GET['wallly_hide_footer']) ? 'hidden' : ''; ?>">
     <div class="row">
       <div class="col-sm-4">
         <ul class="list-inline footer-credits">
